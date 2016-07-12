d12d23b3995b5650159777e48f70eb6bfd215013
Manuel Lorenz <Manuel Lorenz>
2016-07-12 12:41:46 +0200

    DfBuilder-012: Improved pdf

--- src/main/web/js/buildPdf.js
+++ src/main/web/js/buildPdf.js
@@ -1,9 +1,25 @@
 function buildPdf(value) {
     var pdfContent = value;
     var dd = {
+    		footer: 
+                function(currentPage, pageCount) { return [
+                   {
+                       margin: [60, 0, 60, 0],
+                       canvas: [{ type: 'line', x1: 0, y1: 5, x2: 595-2*60, y2: 5, lineWidth: 1 }]},
+                   {
+                    margin: [60, 0, 60, 0],
+                    table: {
+                        widths: [100,'*', 100],
+                        body: [
+                            [ 'DfBuilder Beta0.1', '', { text: 'Page ' + currentPage.toString() + ' of ' + pageCount, style: 'pts' }]
+                        ]
+        			},
+        			layout: 'noBorders'
+        			}];
+        		},
             content: [
                     createHeader(pdfContent),
-                    hrLine(),
+                    {canvas: [{ type: 'line', x1: 0, y1: 5, x2: 595-2*60, y2: 5, lineWidth: 1 }]},
                     table(pdfContent.lineBattlegroupes, ['name', 'pts']),
                     table(pdfContent.pathfinderBattlegroupes, ['name', 'pts']),
                     table(pdfContent.vanguardBattlegroupes, ['name', 'pts']),
@@ -11,23 +27,37 @@ function buildPdf(value) {
             ],
         	styles: {
         		header: {
-        			fontSize: 15,
+        			fontSize: 13,
         			bold: true
         		},
+        		headerRight: {
+        			fontSize: 13,
+        			bold: true,
+        			alignment: 'right'
+        		},
         		subheader: {
-        			fontSize: 15,
+        			fontSize: 10,
         			bold: true
+        		},
+        		subheaderPts: {
+        			fontSize: 10,
+        			bold: true,
+        			alignment: 'right'
         		},
         		quote: {
         			italics: true
         		},
-        		points: {
+        		headerPts: {
         			alignment: 'right',
-        			bold: true
+        			fontSize: 13
+        		},
+        		pts: {
+        			alignment: 'right'
         		}
         	},
+        	pageMargins: [ 60, 40, 60, 40 ],
 	     	defaultStyle: {
-	     		fontSize: 12,
+	     		fontSize: 10,
 	     	}
         }
     console.log(dd);
@@ -40,7 +70,7 @@ function buildTableBody(data, columns) {
 			var dataRow = [];
 			columns.forEach(function(column) {
 				if (column == 'pts') {
-					dataRow.push(row[column].toString() + 'pts');
+					dataRow.push({text: row[column].toString() + ' pts', style: 'pts' });
 				} else {
 					dataRow.push(row[column].toString());
 				}
@@ -48,7 +78,42 @@ function buildTableBody(data, columns) {
 			})
 		body.push(dataRow);
 	});
-	
+	data.mediumShips.forEach(function(row) {
+		var dataRow = [];
+		columns.forEach(function(column) {
+			if (column == 'pts') {
+				dataRow.push({text: row[column].toString() + ' pts', style: 'pts' });
+			} else {
+				dataRow.push(row[column].toString());
+			}
+			
+		})
+		body.push(dataRow);
+	});
+	data.heavyShips.forEach(function(row) {
+		var dataRow = [];
+		columns.forEach(function(column) {
+			if (column == 'pts') {
+				dataRow.push({text: row[column].toString() + ' pts', style: 'pts' });
+			} else {
+				dataRow.push(row[column].toString());
+			}
+			
+		})
+		body.push(dataRow);
+	});
+	data.superHeavyShips.forEach(function(row) {
+		var dataRow = [];
+		columns.forEach(function(column) {
+			if (column == 'pts') {
+				dataRow.push({text: row[column].toString() + ' pts', style: 'pts' });
+			} else {
+				dataRow.push(row[column].toString());
+			}
+			
+		})
+		body.push(dataRow);
+	});
 	return body;
 }
 
@@ -57,39 +122,56 @@ function createHeader(data) {
 		table: {
 			widths: [50,'*', 200],
 			body: [
-					[ data.faction, data.name, { text: 'Battle', style: 'points' }],
-					[ '', '', { text: data.totalPoints + '/' + data.maxPoints + ' PTS', style: 'points' }]
+					[ { text: data.faction, style: 'header'}, { text: data.name, style: 'header'}, { text: data.gameSize.name, style: 'headerRight' }],
+					[ '', '', { text: data.totalPoints + '/' + data.maxPoints + ' PTS', style: 'headerPts' }]
 			]
 		},
 		layout: 'noBorders'
 	};
 }
 
-function hrLine() {
-	return {
-	    table: {
-	            widths: ['*'],
-	            body: ['']
-	    },
-	    layout: {
-	        hLineWidth: function(i, node) {
-	            return (i === 0 || i === node.table.body.length) ? 0 : 2;
-	        },
-	        vLineWidth: function(i, node) {
-	            return 0;
-	        },
-	    }
-	};
-}
+
+
 
 function table(data, columns) {
 	var a = [];
+	var battleType;
 	data.forEach(function(entry) {
-		var c = { text: entry.battleGroupeType.battleType, style: 'header' };
+		if (entry.battleGroupeType.battleType === 'LINE') {
+			battleType = {
+					color: '#697641',
+					text: 'Linegroup', 
+					style: 'subheader' };
+		} else if (entry.battleGroupeType.battleType == 'VANGUARD') {
+			battleType = {
+					color: '#98741F',
+					text: 'Vanguardgroup', 
+					style: 'subheader' };	
+		} else if (entry.battleGroupeType.battleType == 'PATHFINDER') {
+			battleType = {
+					color: '#915591',
+					text: 'Pathfindergroup', 
+					style: 'subheader' };
+		} else if (entry.battleGroupeType.battleType == 'FLAG') {
+			battleType = {
+					color: '#436BA3',
+					text: 'Flaggroup', 
+					style: 'subheader' };	
+		}
+		var c = {
+				margin: [0, 5, 0, 0],
+				table: {
+					widths: [150,'*', 150],
+					body: [
+							[ battleType, '', {text: entry.points + ' pts', style: 'subheaderPts' }]
+					]
+				},
+				layout: 'noBorders'
+			};
 		a.push(c);
 		var b = {
-			table: { 
-				headerRows: 1,
+			margin: [10, 0, 0, 0],
+			table: {
 				widths: ['*', 100],
 				body: buildTableBody(entry, columns)
 			},
